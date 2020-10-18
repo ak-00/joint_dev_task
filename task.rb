d72b9c36f0e508a38aae6ab6cac125104b149b73
ak <maak000ak@gmail.com>
2020-10-18 20:46:17 +0800

    fixed Q4 & Q7

--- task.rb
+++ task.rb
@@ -37,7 +37,8 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  # 1stA : sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -74,7 +75,8 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array.map!{|number| number.to_i}
+  # 1stA : array.map!{|number| number.to_i}
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
