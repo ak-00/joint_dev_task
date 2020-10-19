144afab7af6d7491658759c4a896ac2390e688e4
ak <maak000ak@gmail.com>
2020-10-19 12:07:38 +0800

    fixed Q8&9 2020.10.19)

--- task.rb
+++ task.rb
@@ -85,6 +85,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map(&:capitalize!)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -94,11 +95,10 @@ end
 
 def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
-  index = 1
+
   # 以下に回答を記載
-  names.each do |name|
-    puts "会員No.#{index}#{name}さん"
-    index += 1
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i}#{name}さん"
   end
 
 end
