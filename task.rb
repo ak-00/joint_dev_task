d1b4731658f0301e9ba6d0beb78fbc7efe2e7fa9
ak <maak000ak@gmail.com>
2020-10-20 08:30:38 +0800

    Q11-13 20201020

--- task.rb
+++ task.rb
@@ -120,13 +120,22 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  # sports.each.with_index(1) do |sport, i|
+  # puts "No#{i} #{sport}"
+  # p sports.uniq
 
+    puts "ユーザーの趣味一覧"
+  sport1 = sports.flatten
+  sport1.uniq.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
+  end 
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  puts data[:user][:name]
 
 end
 
@@ -135,6 +144,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  user_data.merge!(update_data)
+  puts user_data
 
 end
 
